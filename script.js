5ce5ed2e3a4bafd8ede130640130f703b2381f0b
Dante <dantebusta@gmail.com>
2025-07-07 19:54:17 +0200

    Added Swedish time to the front page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const projectLink = document.querySelectorAll(".projectDesignFt");
 
-projectLink.forEach(link => {
+projectLink.forEach((link) => {
   link.addEventListener("mouseover", () => {
     link.style.transform = "translateX(10px)";
     link.style.transition = "transform 0.3s ease";
@@ -10,3 +10,14 @@ projectLink.forEach(link => {
     link.style.transform = "translateX(0)";
   });
 });
+
+const timezone = "Europe/Stockholm";
+const now = new Date();
+
+const currentTime = new Intl.DateTimeFormat("en-US", {
+  timeZone: timezone,
+  timeStyle: "short",
+  hour12: false,
+}).format(now);
+
+document.querySelector(".timeFooter").textContent = "CEST (Stockholm time) " + currentTime;
